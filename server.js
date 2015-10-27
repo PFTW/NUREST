964784486c95a692ad38fe20ad47328e4c84220d
rguthmann <riesguthmann@gmail.com>
2015-10-27 12:52:29 -0500

    REST API Keynotes
    REST , GET & POST Keynotes schema

--- server.js
+++ server.js
@@ -1,24 +1,53 @@
-var express = require('express');
-var app = express();
-var fs = require("fs");
+var express     =   require("express");
+var app         =   express();
+var bodyParser  =   require("body-parser");
+var router      =   express.Router();
+var mongoOp     =   require("./models/mongo");
 
-app.get('/listKeynotes', function (req, res) {
-   fs.readFile( __dirname + "/" + "keynotes.json", 'utf8', function (err, data) {
-       console.log( data );
-       res.end( data );
-   });
-})
+ app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded({"extended" : false}));
 
-var server = app.listen(8081, function () {
+ router.get("/",function(req,res){
+     res.json({"error" : false,"message" : "Greetings from Law Circuit 2015"});
+});
 
-  var host = server.address().address
-  var port = server.address().port
+ router.route("/keynotes")
+     .get(function(req,res){
+         var response = {};
+         mongoOp.find({},function(err,data){
+         // Mongo command to fetch all data from collection.
+             if(err) {
+                 response = {"error" : true,"message" : "Error fetching data"};
+             } else {
+                 response = {"error" : false,"message" : data};
+             }
+             res.json(response);
+         });
+          })
+         .post(function(req,res){
+         var db = new mongoOp();
+         var response = {};
+         // fetch keynote attribute
+         db.ktitle = req.body.ktitle; 
+         db.presenter = req.body.presenter; 
+         db.profession = req.body.profession; 
+         db.sessionID = req.body.sessionID; 
+         db.date = req.body.date; 
+         db.duration = req.body.duration; 
+         db.save(function(err){
+         // save() will run insert() command of MongoDB.
+         // it will add new data in collection.
+             if(err) {
+                 response = {"error" : true,"message" : "Error adding data"};
+             } else {
+                 response = {"error" : false,"message" : "Data added"};
+             }
+             res.json(response);
+         });
 
-  console.log("listKeynotes app, host, port)
+     });
 
-})
+ app.use('/',router);
 
-
-//keynotes.json = restful db 
-//addKeynote - 
-
+ app.listen(3000);
+ console.log("Listening to PORT 3000");

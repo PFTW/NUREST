72122d7cc66cdc969144ea0f7ca89bdbb59155b7
rguthmann <riesguthmann@gmail.com>
2015-11-02 20:20:22 -0600

    try it again

--- server.js
+++ server.js
@@ -1,26 +1,21 @@
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Basic HTTP Version - NU CONF 395\n');
-}).listen(port);
+//var http = require('http')
+//var port = process.env.PORT || 1337;
+//http.createServer(function(req, res) {
+//  res.writeHead(200, { 'Content-Type': 'text/plain' });
+//  res.end('Hello Basic HTTP Version - NU CONF 395\n');
+//}).listen(port);
 
+var express = require('express');
+var app = express();
 
+app.get('/', function (req, res) {
+  res.send('Hello Basic HTTP Version - NU CONF 395\n');
+});
 
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
 
-//var express = require('express');
-//var app = express();
-//var fs = require("fs");
+  console.log('Example app listening at http://%s:%s', host, port);
+});
 
-//app.get('/listKeynotes', function (req, res) {
-//   fs.readFile( __dirname + "/" + "keynotes.json", 'utf8', function (err, data) {
-//       console.log( data );
- //      res.end( data );
- //  });
-//})
-
-// app.listen(3000);
-// console.log("Listening to PORT 3000");
-
-
-//keynotes.json = restful db
